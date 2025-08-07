903b53394e0dc68503e04b22d54fd4d99d8f45af
Bao-Trinh-Quoc <bao.trinhquoc@hcmut.edu.vn>
2025-08-07 07:09:27 +0700

    feat: add colorschememe, modify stylua

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+	"folke/tokyonight.nvim",
+	lazy = true,
+	priority = 1000,
+	opts = {
+		transparent = true,
+		on_highlights = function(hl)
+			-- hl.LineNr = { fg = "#b2b8cf", bold = true }
+			hl.LineNrAbove = { fg = "#F5E57D", bold = true }
+			hl.LineNr = { fg = "#DED4E8", bold = true }
+			hl.LineNrBelow = { fg = "#DED4E8", bold = true }
+		end,
+		styles = {
+			sidebars = "transparent",
+			floats = "transparent",
+		},
+	},
+}
